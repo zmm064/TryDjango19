53a7e387cf77ffe8da5c50f49a1f345647cacf62
xuhui <zmm064@foxmail.com>
2018-03-09 17:37:44 +0800

    课时12：筛选过滤返回的数据

--- posts/api/views.py
+++ posts/api/views.py
@@ -5,12 +5,23 @@ from rest_framework.generics import (
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
 from posts.models import Post
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .serializers import PostListSerializer, PostDetailSerializer, PostCeateSerializer
 from .permissions import IsOwnerOrReadOnly
 
 class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
+    # queryset = Post.objects.all()
     serializer_class = PostListSerializer
+    filter_backends = [SearchFilter,    # 通过search查询过滤
+                       OrderingFilter]  # 通过ordering查询过滤
+    search_fields = ['title', 'content', 'user__first_name']
+
+    def get_queryset(self, *args, **kwargs):
+        queryset_list = Post.objects.all()
+        query = self.request.GET.get('q')  # 通过q查询过滤
+        if query:
+            queryset_list = queryset_list.filter(title__icontains=query)
+        return queryset_list
 
 
 class PostCreateAPIView(CreateAPIView):
