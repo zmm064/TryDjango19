54d7dfd9ec1045efb14778dbee2517e413f5874f
xuhui <zmm064@foxmail.com>
2018-03-09 10:26:01 +0800

    课时6：创建提交详情API

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.PostListAPIView.as_view(), name='list'),
-    #url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
-    #url(r'^(?P<slug>[\w-]+)/$', views.post_detail, name='detail'),
+    url(r'^(?P<slug>[\w-]+)/$', views.PostDetailAPIView.as_view(), name='detail'),
     #url(r'^(?P<slug>[\w-]+)/edit$', views.post_update, name='update'),
     #url(r'^create$', views.post_create),
     #url(r'^(?P<slug>[\w-]+)/delete$', views.post_delete, name='delete'),
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,7 +1,13 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from posts.models import Post
-from .serializers import PostSerializer
+from .serializers import PostListSerializer, PostDetailSerializer
 
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostListSerializer
+
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
